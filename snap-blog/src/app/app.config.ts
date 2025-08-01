0b9d57232b16f7c46199cb7243ac965d02126e62
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-02 00:07:16 +0300

    Created the angular file and added some of the firebase configuration.

---
+++ snap-blog/src/app/app.config.ts
@@ -0,0 +1,32 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDs0ytf2No4tLuMwBnueZT1VATLkn5KqHA',
+  authDomain: 'softuni-blog-project.firebaseapp.com',
+  projectId: 'softuni-blog-project',
+  storageBucket: 'softuni-blog-project.firebasestorage.app',
+  messagingSenderId: '636311296591',
+  appId: '1:636311296591:web:961442cc761399b860e83a',
+};
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+  ],
+};
