15bcf2065d9fde6b83f1997d1d59c8c2c51f64ad
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-04 21:47:55 +0300

    Created login component and added it as a route

--- src/app/features/authentication/login/login.ts
+++ src/app/features/authentication/login/login.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthenticationService } from '../../../core/services';
+import { Router } from '@angular/router';
+import { FormBuilderService } from '../../../core/services/formBuilder.service';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  private authService=inject(AuthenticationService)
+  private router=inject(Router)
+  public formBuilderService=inject(FormBuilderService)
 
+  loginForm:FormGroup;
+
+  constructor(){
+    this.loginForm=this.formBuilderService.createForm(2)
+  }
+
+  get isEmailValid(): boolean {
+   return this.formBuilderService.isEmailError(this.loginForm)
+  }
+
+  get isPasswordValid(): boolean {
+    return this.formBuilderService.isLoginPasswordError(this.loginForm)
+  }
+
+  get emailErrorMessage(): string {
+    return this.formBuilderService.getEmailErrorMessage(this.loginForm)
+  }
+
+  get passwordErrorMessage(): string {
+    return this.formBuilderService.getLoginPasswordErrorMessage(this.loginForm)
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      const response=this.authService.login(email,password)
+
+      if (response) {
+        this.router.navigate(['']);
+      }
+    }
+  }
 }
