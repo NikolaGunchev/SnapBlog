e3b4f4e270b49c1ecbf3d06a5944904d9ad6aa05
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-07 14:38:07 +0300

    Made the general layout of the home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./features/home/home').then(c=>c.Home),
+  },
+  {
     path: 'login',
     loadComponent: () => import('./features/authentication/login/login').then(c=>c.Login),
   },
