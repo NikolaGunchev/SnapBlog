fbe3f725e792e221c967f9e292de3e1822c625bc
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-08 16:10:00 +0300

    Minor tweaks

--- src/app/features/post-item/post-item.ts
+++ src/app/features/post-item/post-item.ts
@@ -2,19 +2,18 @@ import {
   Component,
   inject,
   Input,
-  OnDestroy,
   OnInit,
-  Signal,
 } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Group, Post } from '../../model';
 import { GroupsService } from '../../core/services/groups.service';
 import { CommonModule } from '@angular/common';
 import { TimeAgoPipe } from '../../shared/pipes/time-ago-pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-item',
-  imports: [CommonModule, TimeAgoPipe],
+  imports: [CommonModule, TimeAgoPipe, RouterLink],
   templateUrl: './post-item.html',
   styleUrl: './post-item.css',
 })
