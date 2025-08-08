0f9b69a982dd95f6c06d78ff793d0fd0e701840f
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-08 14:43:24 +0300

    added dropdown component

---
+++ src/app/shared/dropdown/dropdown.ts
@@ -0,0 +1,12 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  imports: [],
+  templateUrl: './dropdown.html',
+  styleUrl: './dropdown.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class Dropdown {
+  
+}
