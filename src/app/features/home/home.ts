745ddb59a2fbbcdc1ff2901361150f15a827f6f2
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-08 14:43:51 +0300

    Finished the posts part in the home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -4,10 +4,11 @@ import { Post } from '../../model';
 import { Observable } from 'rxjs';
 import { PostItem } from '../post-item/post-item';
 import { CommonModule } from '@angular/common';
+import { Dropdown } from '../../shared/dropdown/dropdown';
 
 @Component({
   selector: 'app-home',
-  imports: [PostItem, CommonModule],
+  imports: [PostItem, CommonModule, Dropdown],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
--- src/app/features/post-item/post-item.ts
+++ src/app/features/post-item/post-item.ts
@@ -1,5 +1,12 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  inject,
+  Input,
+  OnDestroy,
+  OnInit,
+  Signal,
+} from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Group, Post } from '../../model';
 import { GroupsService } from '../../core/services/groups.service';
 import { CommonModule } from '@angular/common';
@@ -9,15 +16,16 @@ import { TimeAgoPipe } from '../../shared/pipes/time-ago-pipe';
   selector: 'app-post-item',
   imports: [CommonModule, TimeAgoPipe],
   templateUrl: './post-item.html',
-  styleUrl: './post-item.css'
+  styleUrl: './post-item.css',
 })
 export class PostItem implements OnInit {
-  private groupService=inject(GroupsService)
-  groupDetail$!:Observable<Group | undefined>
+  private groupService = inject(GroupsService);
 
-  @Input() post!:Post
+  groupDetail$!: Observable<Group | undefined>;
+
+  @Input() post!: Post;
 
   ngOnInit(): void {
-    this.groupDetail$=this.groupService.getGroupById(this.post.groupId);    
+    this.groupDetail$ = this.groupService.getGroupById(this.post.groupId);
   }
 }
