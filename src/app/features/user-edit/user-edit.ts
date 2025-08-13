3c247b7f058459ae09304207d4fe972ce697241e
NikolaGunchev <nikolagunchev69@gmail.com>
2025-08-13 14:58:27 +0300

    Made option to edit user info

--- src/app/features/user-edit/user-edit.ts
+++ src/app/features/user-edit/user-edit.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilderService } from '../../core/services';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilderService, UserService } from '../../core/services';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -8,14 +8,25 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './user-edit.html',
   styleUrl: './user-edit.css'
 })
-export class UserEdit {
+export class UserEdit implements OnInit {
    private formBuilderService=inject(FormBuilderService)
+   private userService=inject(UserService)
 
    userForm:FormGroup
+   currentProfile=this.userService.userProfile
 
    constructor(){
     this.userForm=this.formBuilderService.createForm(3)
    }
+
+  ngOnInit(): void {
+    const data={
+      username:this.currentProfile()?.username,
+      bio:this.currentProfile()?.bio
+    }
+
+    this.userForm.patchValue(data)
+  }
 
    get isUsernameInvalid():boolean{
     return this.formBuilderService.isUsernameError(this.userForm)
